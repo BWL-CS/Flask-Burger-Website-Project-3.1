4e872d5110acda9d31fcbe2c58f5252a820900d8
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2025-01-22 15:41:31 +0000

    hohoaha

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ def index():
 def hello_there(name_data = None):
     return render_template("hello_there.html", name=name_data)
 
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+ # Replace the existing index function with the one below
+
+ # New functions
+@app.route("/about/")
+def about():
+     return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+     return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5421)
 
